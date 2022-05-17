bcf6ec25c307ed3699e4bfbbe86fbb2cfe53dbce
Joan Cerretani <joancerretani@gmail.com>
2022-05-17 16:41:55 -0300

    First commit

---
+++ neural_response_map/__init__.py
@@ -0,0 +1,1 @@
+from neural_response_map.mappers import NeuralResponseMap
---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1'
+PACKAGE_NAME = 'neural_response_map'
+AUTHOR = 'Joan Alberto Cerretani'
+AUTHOR_EMAIL = 'joancerretani@gmail.com'
+URL = 'https://github.com/joancerretani'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Library to visualize the activations of the hidden layers of artificial neural networks'
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
+LONG_DESC_TYPE = "text/markdown"
+
+with Path('requirements.txt').open() as requirements_txt:
+    INSTALL_REQUIRES = [str(requirement) for requirement in pkg_resources.parse_requirements(requirements_txt)]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
